93d83eb2edf1d3eba2a6e43b1ca4fbfb9d63cdf2
Rohit Gulam <rohitsgulam@gmail.com>
2022-03-02 12:17:27 +0300

    fixed db config

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,19 +1,19 @@
 <?php
 
-// $serverName = "localhost";
-// $dBUserName = "rohit";
-// $dBPassword = "wA</0083uzlk1669";
-// $dBName = "qrmenu";
-// $portNumber = 3306;
-
-$serverName = "173.255.211.248";
-$dBUserName = "r0hitgulam";
+$serverName = "localhost";
+$dBUserName = "rohit";
 $dBPassword = "wA</0083uzlk1669";
 $dBName = "qrmenu";
 $portNumber = 3306;
 
+// $serverName = "173.255.211.248";
+// $dBUserName = "r0hitgulam";
+// $dBPassword = "wA</0083uzlk1669";
+// $dBName = "qrmenu";
+// $portNumber = 3306;
+
 $conn = mysqli_connect($serverName, $dBUserName, $dBPassword, $dBName);
-$pdo = new PDO('mysql:host=173.255.211.248;port=3306;dbname=qrmenu', $dBUserName, $dBPassword);
+$pdo = new PDO('mysql:host=localhost;port=3306;dbname=qrmenu', $dBUserName, $dBPassword);
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 if(!$conn){
